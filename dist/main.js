577384fdf2716f26c1d0601a76566bfcc5dbcf01
Darzhan <eduardovdarzhan@gmail.com>
2025-01-16 03:15:23 +0500

    minor changes

--- dist/main.js
+++ dist/main.js
@@ -2,8 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const fs = require("fs");
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const httpsOptions = {
+        key: fs.readFileSync('./secrets/privkey.pem'),
+        cert: fs.readFileSync('./secrets/fullchain.pem'),
+    };
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, { httpsOptions });
     app.enableCors({
         origin: true,
         credentials: true,
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import * as fs from 'fs';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('./secrets/privkey.pem'),
+    cert: fs.readFileSync('./secrets/fullchain.pem'),
+  };
+  const app = await NestFactory.create(AppModule, { httpsOptions });
 
   app.enableCors({
     origin: true,
